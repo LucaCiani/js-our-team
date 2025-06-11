fd10af5e561f1bedca547e1e574f3b6c13e5ea97
LucaCiani <lucaciani88@gmail.com>
2025-06-11 15:20:53 +0200

    add js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,36 +3,66 @@ const teamMembers = [
         name: "Marco Bianchi",
         role: "Designer",
         email: "marcobianchi@team.com",
-        img: "img/male1.png",
+        img: "male1.png",
     },
     {
         name: "Laura Rossi",
         role: "Front-end Developer",
         email: "laurarossi@team.com",
-        img: "img/female1.png",
+        img: "female1.png",
     },
     {
         name: "Giorgio Verdi",
         role: "Back-end Developer",
         email: "giorgioverdi@team.com",
-        img: "img/male2.png",
+        img: "male2.png",
     },
     {
         name: "Marta Ipsum",
         role: "SEO Specialist",
         email: "martarossi@team.com",
-        img: "img/female2.png",
+        img: "female2.png",
     },
     {
         name: "Roberto Lorem",
         role: "SEO Specialist",
         email: "robertolorem@team.com",
-        img: "img/male3.png",
+        img: "male3.png",
     },
     {
         name: "Daniela Amet",
         role: "Analyst",
         email: "danielaamet@team.com",
-        img: "img/female3.png",
+        img: "female3.png",
     },
 ];
+
+const teamInfo = document.getElementById("team-info");
+
+const printInfo = () => {
+    for (let i = 0; i < teamMembers.length; i++) {
+        const member = teamMembers[i];
+        teamInfo.innerHTML += `<div class="card mb-3">
+                        <div class="row g-0">
+                            <div class="col-4">
+                                <img
+                                    src="../assets/img/${member.image}"
+                                    class="img-fluid rounded-start"
+                                    alt="${member.name}"
+                                />
+                            </div>
+                            <div class="col-8">
+                                <div class="card-body bg-dark text-light">
+                                    <h5 class="card-title">${member.name}</h5>
+                                    <p class="card-text">
+                                        <p>${member.role}</p>
+                                        <a href="#">${member.email}</a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`;
+    }
+};
+
+printInfo();
